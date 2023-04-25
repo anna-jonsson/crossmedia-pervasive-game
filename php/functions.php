04c05db068b41948b8dc1e7d26fddbcd1ff999f5
mirandalyng <lyngmiranda@gmail.com>
2023-04-25 13:34:35 +0200

    Functions and passwords php-files+ code

---
+++ php/functions.php
@@ -0,0 +1,37 @@
+<?php
+
+//Request method
+$request_method = $_SERVER["REQUEST_METHOD"];
+
+//Requested JSON 
+$r_json = file_get_contents("php://input");
+
+//Requested data 
+$r_data = json_decode($r_json, true);
+
+//Array of places 
+$places = [];
+
+//The database for locations/places, and their information 
+$placeDatabase = "../JSON/placeDatabase.json";
+
+//If the placeDatabase.json exists update and decode it
+
+if(file_exists($placeDatabase)){
+    $places_json = file_get_contents($placeDatabase);
+    $places = json_decode($places_json, true);
+}
+
+//General function for sending the data + statusCode (default 200). Run the exit() method in the end so this doesn't have to be done in the other code. 
+function applyJSON($data, $statusCode = 200) {
+   header('Content-Type: application/json');
+   http_response_code($statusCode);
+   echo json_encode($data);
+   exit();
+}
+
+
+?>
+
+
+

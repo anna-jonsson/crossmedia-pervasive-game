8a891415a7de44663caab95449cc92db8beb12ef
anna-jonsson <anna.f.jonsson@gmail.com>
2023-04-25 16:18:18 +0200

    patch for checked in & checked out works now

--- php/balance.php
+++ php/balance.php
@@ -7,14 +7,8 @@ if($request_method != "PATCH") {
 
 foreach($places as $place){
 
-    if(file_exists($placeDatabase, $r_data["place"], $r_data["balance"])){
-        $places_json = file_get_contents($placeDatabase);
-        $places = json_decode($places_json, true);
-
     if ($r_data["location_name"] == $place["location_name"]) {
         $place["balance"] = $r_data["balance"];
-    }
-
     }
 }
 

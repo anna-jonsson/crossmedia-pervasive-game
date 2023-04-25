4acbc01bf0710acc1de234211961e729408e1901
anna-jonsson <anna.f.jonsson@gmail.com>
2023-04-25 13:39:31 +0200

    fixed spelling error

--- php/functions.php
+++ php/functions.php
@@ -13,7 +13,7 @@ $r_data = json_decode($r_json, true);
 $places = [];
 
 //The database for locations/places, and their information 
-$placeDatabase = "../JSON/placeDatabase.json";
+$placeDatabase = "../json/placeDatabase.json";
 
 //If the placeDatabase.json exists update and decode it
 
@@ -23,7 +23,7 @@ if(file_exists($placeDatabase)){
 }
 
 //General function for sending the data + statusCode (default 200). Run the exit() method in the end so this doesn't have to be done in the other code. 
-function applyJSON($data, $statusCode = 200) {
+function sendJSON($data, $statusCode = 200) {
    header('Content-Type: application/json');
    http_response_code($statusCode);
    echo json_encode($data);

da02455e36a29a7cc754561e475242b3f1e5023e
mirandalyng <lyngmiranda@gmail.com>
2023-04-25 13:40:32 +0200

    Update checked_in_out.php

--- php/checked_in_out.php
+++ php/checked_in_out.php
@@ -10,16 +10,27 @@ if($request_method != "PATCH"){
 }
 
 // if requested place matches place in JSON & has NOT been checked in to before, set checked_in to true.
+<<<<<<< Updated upstream
 if($place["location_name"] == $request_data["location_name"] and    
 $place["checked_in"] == false) {
+=======
+if($place["location_name"] == $r_data["location_name"] and    
+    $place["checked_in"] == false) {
+>>>>>>> Stashed changes
 	$place["checked_in"] == true; 
     sendJSON($place);
 }
 
 // if requested place matches place in JSON & has NOT been checked out, set checked_out to true.
+<<<<<<< Updated upstream
 if($place["location_name"] == $request_data["location_name"] and    
 $place["checked_out"] == false) {
 	place["checked_out"] == true; 
+=======
+if($place["location_name"] == $r_data["location_name"] and    
+    $place["checked_out"] == false) {
+	$place["checked_out"] == true; 
+>>>>>>> Stashed changes
     sendJSON($place);
 }
 

d69893f6bf40844c1c88a6099beda52c02ae9768
anna-jonsson <anna.f.jonsson@gmail.com>
2023-04-26 14:04:28 +0200

    balance up & running

--- php/balance.php
+++ php/balance.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once "functions.php";
+
 //Verification that the request method is PATCH 
 //If not send error 
 if($request_method != "PATCH") {
@@ -9,13 +11,14 @@ if($request_method != "PATCH") {
 
 
 //Checking that the request data includes the parameter current balance
-if(isset($r_data["current_balance"])) {
+if(isset($r_data["current_balance"]) && isset($r_data["location_name"])) {
     //Loops through the array /json and using index 
     foreach($places as $placeIndex => $place){
         //Checking if request data location is the same as the current location 
         if ($r_data["location_name"] == $place["location_name"]) {
             //Updates the current balance 
-            $place["current_balance"] .= $r_data["current_balance"];
+            $sum = $place["current_balance"] + $r_data["current_balance"];
+            $place["current_balance"] = $sum;
 
             $updatedPlace = $place;
 

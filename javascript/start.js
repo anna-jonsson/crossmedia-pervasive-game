2172787fa38e145c95d64e4f77880cc8f09bfb66
mirandalyng <lyngmiranda@gmail.com>
2023-04-26 12:49:03 +0200

    Create start.js

---
+++ javascript/start.js
@@ -0,0 +1,29 @@
+//Function for the introduction in the game. 
+const array_of_objects = "../json.placDatabase.json";
+function introduction () { 
+	main.innerHTML = div + button ('nästa')
+	nästa.eventlistener(introLock(main))
+}
+//Function for locking up the first password with eventListner for the input showing the map afterwards. 
+function introLock (array_of_objects) {
+	inputPassword
+
+
+eventlistener:
+password_request(password.text)
+Show_map()
+}
+
+	// anropas för varje plats, fyller med olika innehåll
+function fill_content(array_of_objects, array_index, div_id) {
+	div_id.innerHTML = `[gåta + nästa-knapp +
+	uppgiftsbeskrivning + lösenord ]`
+	
+	eventlistener:
+	nextBtn (från gåta)
+	QR_handling (om sådan ska scannas vid uppgiftsplats)
+	password_request(password.text)
+
+	if(password_request)
+
+}

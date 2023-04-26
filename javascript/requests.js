3b3e823b30bd081209a35267eb17854c062cf48b
mirandalyng <lyngmiranda@gmail.com>
2023-04-26 22:20:50 +0200

    patch-requests done in js for checked in and out and balance
    DId some changes in the js code request. Also added a thought to the checked in and checked out maybe should be the same key

--- javascript/requests.js
+++ javascript/requests.js
@@ -1,18 +1,71 @@
+//GET
+//Function for place_request with the placeName as parameter.
 function place_request(placeName) {
-    const placeRequest = new Request("../php/get_location.php?location_name=" + placeName);
+  const placeRequest = new Request("../php/get_location.php?location_name=" + placeName);
 
-    fetch(placeRequest)
-        .then(resp => resp.json())
-        // TODO: switch console.log to actual action
-        .then(rsc => console.log(rsc));
+  fetch(placeRequest)
+    .then((resp) => resp.json())
+    // TODO: switch console.log to actual action
+    .then((rsc) => console.log(rsc));
+}
+//Function for check_password with the placeName and password as parameter.
+function check_password(placeName, password) {
+  const passwordRequest = new Request(
+    "../php/passwords.php?password=" + password + "&location_name=" + placeName
+  );
 
+  fetch(passwordRequest)
+    .then((resp) => resp.json())
+    // TODO: switch console.log to actual action
+    .then((rsc) => console.log(rsc));
 }
 
-function check_password(placeName, password) {
-    const passwordRequest = new Request("../php/passwords.php?password=" + password + "&location_name=" + placeName);
+//PATCH
+//Function checking in - PATCH method
+function checked_in(placeName, checked_status) {
+  const checked_inRequest = new Request("../php/checked_in_out.php");
+  fetch(checked_inRequest, {
+    method: "PATCH",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      location_name: placeName,
+      checked_in: checked_status,
+    }),
+  })
+    .then((r) => r.json())
+    // TODO: switch console.log to actual action
+    .then((rsc) => console.log(rsc));
+}
+//Function for checking out - PATCH method
+function checked_out(placeName, checked_status) {
+  const checked_outRequest = new Request("../php/checked_in_out.php");
+  fetch(checked_outRequest, {
+    method: "PATCH",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      location_name: placeName,
+      checked_out: checked_status,
+    }),
+  })
+    .then((r) => r.json())
+    // TODO: switch console.log to actual action
+    .then((rsc) => console.log(rsc));
+}
 
-    fetch(passwordRequest)
-        .then(resp => resp.json())
-        // TODO: switch console.log to actual action
-        .then(rsc => console.log(rsc));
+//FUNDERING!!! - ska inte checked status vara nyckeln så att det är den som ändras och inte out och in som olika??
+
+//Function for balance request
+function patch_balance(placeName, balance) {
+  const balanceRequest = new Request("../php/balance.php");
+  fetch(balanceRequest, {
+    method: "PATCH",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      location_name: placeName,
+      current_balance: balance,
+    }),
+  })
+    .then((r) => r.json())
+    // TODO: switch console.log to actual action
+    .then((rsc) => console.log(rsc));
 }

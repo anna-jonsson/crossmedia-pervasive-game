cb3e2b70053ac29592a3c179817d82448dc867bc
anna-jonsson <anna.f.jonsson@gmail.com>
2023-04-27 14:43:34 +0200

    location function + start look

--- javascript/index.js
+++ javascript/index.js
@@ -1,7 +1,13 @@
+// re-useable fn to fill content with 
 async function fill_content(placeName, div_id, type) {
 
     let location = await place_request(placeName);
     let wrapper = document.getElementById(div_id);
+    wrapper.style.backgroundImage = `url(../images/${location.background_picture})`;
+    wrapper.style.backgroundSize = 'cover';
+    wrapper.style.width = '400px';
+    wrapper.style.height = '600px';
+
     wrapper.innerHTML = `
         <div class='location'>
             <div class='locationText'>${location.intro_text}</div>
@@ -21,12 +27,41 @@ async function fill_content(placeName, div_id, type) {
             wrapper.innerHTML = ` 
             <div class='task'>
                 <div class='taskText'>${location.task_text}</div>
-                <input type=${type}></input>
-                <button class='taskBtn'>Skicka svar</button>
+                <input class='pw_input' type=${type}></input>
+                <button class='pwBtn'>Skicka svar</button>
             </div>
             `;
+
+            let btnPassword = document.querySelector('.pwBtn');
+            btnPassword.addEventListener('click', async function () {
+                let password = document.querySelector('.pw_input').value;
+                let passwordCheck = await check_password(placeName, password);
+                user_feedback(passwordCheck);
+
+            });
         });
 
     });
+
 }
+
+function user_feedback(response) {
+
+    let wrong_input = "Det är fel lösenord. Vänligen försök igen";
+    let server_error, default_error = "Ooops! Något gick fel, prova igen!";
+    let correct_input = "Grattis, ni klarade det!";
+
+    //Kanske byta ut response till type
+
+    if (response == 500) {
+        alert(server_error);
+    } else if (response == 400) {
+        alert(wrong_input);
+    } else if (response == 200) {
+        alert(correct_input);
+    } else {
+        alert(default_error);
+    }
+}
+
 

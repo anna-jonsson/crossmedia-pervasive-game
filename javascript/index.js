0221b9e3c6a6ca60c91aa12f62fa6849dcf1d2a8
MaxErixon <maxerixon112@gmail.com>
2023-05-05 09:59:10 +0200

    Initial code

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,5 @@
+// import startup from "wordle.js";
+
 // re-useable fn to fill div content based on the location_name, div id and type ("text, password, checkbox etc ")
 async function fill_content(placeName, div_id, type) {
   let location = await place_request(placeName);
@@ -18,34 +20,45 @@ async function fill_content(placeName, div_id, type) {
     `;
 
   //Button for the riddle, showing the riddle text when clicking it
-  let btnRiddle = document.querySelector(".nextBtn").addEventListener("click", function () {
-    wrapper.innerHTML = `
+  let btnRiddle = document
+    .querySelector(".nextBtn")
+    .addEventListener("click", function () {
+      wrapper.innerHTML = `
             <div class='riddle'>
                 <div class='riddleText'>${location.riddle_text}</div>
                 <button class='taskBtn'>Jag har hittat dit!</button>
             </div>
         `;
-    //Button for showing the task text with the type (password, checkbox etc.)
-    let btnTask = document.querySelector(".taskBtn").addEventListener("click", function () {
-      wrapper.innerHTML = ` 
-            <div class='task'>
-                <div class='taskText'>${location.task_text}</div>
-                <input class='pw_input' type=${type}></input>
-                <button class='pwBtn'>Skicka svar</button>
-            </div>
-            `;
-      //Checking that the password for the task is correct with funciton check_password.
-      let btnPassword = document.querySelector(".pwBtn");
-      btnPassword.addEventListener("click", async function () {
-        let password = document.querySelector(".pw_input").value;
-        let passwordCheck = await check_password(placeName, password);
-        //Sending feedback to the user based on the input (correct/incorrect)
-        user_feedback(passwordCheck, placeName);
+      //Button for showing the task text with the type (password, checkbox etc.)
+      let btnTask = document
+        .querySelector(".taskBtn")
+        .addEventListener("click", function () {
+          if (placeName == "möllan") {
+            wrapper.innerHTML="";
+            startup();
+          }
+          else{
 
-        await add_to_balance(placeName, password);
-      });
+            wrapper.innerHTML = ` 
+              <div class='task'>
+                  <div class='taskText'>${location.task_text}</div>
+                  <input class='pw_input' type=${type}></input>
+                  <button class='pwBtn'>Skicka svar</button>
+              </div>
+              `;
+            //Checking that the password for the task is correct with funciton check_password.
+            let btnPassword = document.querySelector(".pwBtn");
+            btnPassword.addEventListener("click", async function () {
+              let password = document.querySelector(".pw_input").value;
+              let passwordCheck = await check_password(placeName, password);
+              //Sending feedback to the user based on the input (correct/incorrect)
+              user_feedback(passwordCheck, placeName);
+  
+              await add_to_balance(placeName, password);
+            });
+          }
+        });
     });
-  });
 }
 
 //Function for user feedback based on the response status connected to the location name.

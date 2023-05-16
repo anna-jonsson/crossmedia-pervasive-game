c49acfc55a9662f160dddd4402d26222c918f80f
mirandalyng <lyngmiranda@gmail.com>
2023-05-16 21:37:22 +0200

    login etc

--- javascript/index.js
+++ javascript/index.js
@@ -18,58 +18,46 @@ async function fill_content(placeName, div_id, type) {
     `;
 
   //Button for the riddle, showing the riddle text when clicking it
-  let btnRiddle = document
-    .querySelector(".nextBtn")
-    .addEventListener("click", function () {
-      wrapper.innerHTML = `
+  let btnRiddle = document.querySelector(".nextBtn").addEventListener("click", function () {
+    wrapper.innerHTML = `
             <div class='riddle'>
                 <div class='riddleText'>${location.riddle_text}</div>
                 <button class='taskBtn'>Jag har hittat dit!</button>
             </div>
         `;
 
-      //Button for showing the task text with the type (password, checkbox etc.)
-      let btnTask = document
-        .querySelector(".taskBtn")
-        .addEventListener("click", async function () {
-          if (placeName == "möllan") {
-            wrapper.innerHTML = "";
-            startup();
+    //Button for showing the task text with the type (password, checkbox etc.)
+    let btnTask = document.querySelector(".taskBtn").addEventListener("click", async function () {
+      if (placeName == "möllan") {
+        wrapper.innerHTML = "";
+        startup();
 
+        document.getElementById("wordleBtn").addEventListener("click", function () {
+          document.querySelector(".grid").style.backgroundColor = "black";
+          document.querySelector(".grid").style.backgroundImage = "none";
 
-            document.getElementById("wordleBtn").addEventListener("click", function () {
-                document.querySelector(".grid").style.backgroundColor = "black";
-                document.querySelector(".grid").style.backgroundImage = "none";
+          document.getElementById("keyboard-cont").style.display = "flex";
 
-                document.getElementById("keyboard-cont").style.display="flex";
+          document.getElementById("wordleBtn").style.display = "none";
 
-                document.getElementById("wordleBtn").style.display = "none";
+          document.getElementById("game").style.flexDirection = "column-reverse";
 
-                document.getElementById("game").style.flexDirection="column-reverse";
-                
+          // document.getElementById("game").innerHTML =`
+          // <h1>Hej</h1>
+          // `;
 
-                // document.getElementById("game").innerHTML =`
-                // <h1>Hej</h1>
-                // `;
-
-                const boxes = document.querySelectorAll(".box");
-                boxes.forEach((box) => {
-                  box.style.visibility = "visible";
-                });
-              });
-          }
-
-           else if (placeName == "triangeln") {
-
-            document
-              .querySelector("#mainContent")
-              .classList.add("snakeContain");
-            init_snake_game();
-          } else if (placeName == "friisgatan") {
-            window.location.href = "../html/pattern.html";
-
-          } else {
-            wrapper.innerHTML = ` 
+          const boxes = document.querySelectorAll(".box");
+          boxes.forEach((box) => {
+            box.style.visibility = "visible";
+          });
+        });
+      } else if (placeName == "triangeln") {
+        document.querySelector("#mainContent").classList.add("snakeContain");
+        init_snake_game();
+      } else if (placeName == "friisgatan") {
+        window.location.href = "../html/pattern.html";
+      } else {
+        wrapper.innerHTML = ` 
               <div class='task' >
                   <div class='taskText'>${location.task_text}</div>
                   <input class='pw_input' type=${type}></input>
@@ -77,42 +65,42 @@ async function fill_content(placeName, div_id, type) {
               </div>
               `;
 
-            if (placeName != "knarkrondellen") {
-              //Checking that the password for the task is correct with funciton check_password.
-              let btnPassword = document.querySelector(".pwBtn");
-              btnPassword.addEventListener("click", async function () {
-                let password = document.querySelector(".pw_input").value;
+        if (placeName != "knarkrondellen") {
+          //Checking that the password for the task is correct with funciton check_password.
+          let btnPassword = document.querySelector(".pwBtn");
+          btnPassword.addEventListener("click", async function () {
+            let password = document.querySelector(".pw_input").value;
 
-                let passwordCheck = await check_password(placeName, password);
-                //Sending feedback to the user based on the input (correct/incorrect)
-                user_feedback(passwordCheck, placeName);
+            let passwordCheck = await check_password(placeName, password);
+            //Sending feedback to the user based on the input (correct/incorrect)
+            user_feedback(passwordCheck, placeName);
 
-                await add_to_balance(placeName, password);
-              });
-            } else {
-              document.querySelector("#mainContent");
-              // input_fields();
-              document.querySelector(".pw_input").style.display = "none";
-              await createInputs();
-              await input_fields();
+            await add_to_balance(placeName, password);
+          });
+        } else {
+          document.querySelector("#mainContent");
+          // input_fields();
+          document.querySelector(".pw_input").style.display = "none";
+          await createInputs();
+          await input_fields();
 
-              let btnPassword = document.querySelector(".pwBtn");
-              btnPassword.addEventListener("click", async function () {
-                // document.querySelector("#input_wrapper").innerHTML = "";
-                await input_fields();
+          let btnPassword = document.querySelector(".pwBtn");
+          btnPassword.addEventListener("click", async function () {
+            // document.querySelector("#input_wrapper").innerHTML = "";
+            await input_fields();
 
-                let password = localStorage.getItem("knarkrondellen");
-                console.log(password);
-                let passwordCheck = await check_password(placeName, password);
-                //Sending feedback to the user based on the input (correct/incorrect)
-                user_feedback(passwordCheck, placeName);
+            let password = localStorage.getItem("knarkrondellen");
+            console.log(password);
+            let passwordCheck = await check_password(placeName, password);
+            //Sending feedback to the user based on the input (correct/incorrect)
+            user_feedback(passwordCheck, placeName);
 
-                await add_to_balance(placeName, password);
-              });
-            }
-          }
-        });
+            await add_to_balance(placeName, password);
+          });
+        }
+      }
     });
+  });
 }
 
 //Function for user feedback based on the response status connected to the location name.
@@ -121,21 +109,26 @@ function user_feedback(response, location_name) {
   let server_error,
     default_error = "Ooops! Något gick fel, prova igen!";
   let correct_input = "Grattis, ni klarade det!";
+  let loggedIn = "Välkommen!";
 
   let intro = "Klicka på platsikonen \n för att läsa mer om platsen.";
   let p = document.createElement("p");
   let newDiv = document.createElement("div");
   let newSpan = document.createElement("span");
-  newDiv.classList.add("feedbackPopup");
-  p.textContent =
-    response == 200
-      ? correct_input
-      : response == 400
-      ? wrong_input
-      : response == 500
-      ? server_error
-      : default_error;
 
+  if (response == 200 && location_name == "logIn") {
+    startpage("introduction", "mainContent");
+  } else {
+    newDiv.classList.add("feedbackPopup");
+    p.textContent =
+      response == 200
+        ? correct_input
+        : response == 400
+        ? wrong_input
+        : response == 500
+        ? server_error
+        : default_error;
+  }
 
   if (
     response == 400 ||
@@ -153,7 +146,6 @@ function user_feedback(response, location_name) {
     };
   }
 
-
   newDiv.appendChild(p);
   document.body.appendChild(newDiv);
 
@@ -164,8 +156,7 @@ function user_feedback(response, location_name) {
     }
   };
 
-  if (response == 200) {
-
+  if (response == 200 && location_name != "logIn") {
     if (location_name == "introduction") {
       show_map();
       p.textContent = intro;
